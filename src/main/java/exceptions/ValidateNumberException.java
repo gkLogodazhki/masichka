2c274ce39ec9efe09e34ec7601fe59c13c38ad16
gklogodazhki <gklogodazhki@gmail.com>
2017-10-13 14:08:42 +0300

    add ValidateNumberException

---
+++ src/main/java/exceptions/ValidateNumberException.java
@@ -0,0 +1,36 @@
+package exceptions;
+
+public class ValidateNumberException extends Exception {
+
+    public ValidateNumberException() {
+    }
+
+    public ValidateNumberException(String message) {
+        super(message);
+    }
+
+    public ValidateNumberException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ValidateNumberException(Throwable cause) {
+        super(cause);
+    }
+
+    public static int validateInteger(int number, int min, int limit) throws ValidateNumberException {
+        if (number >= min && number <= limit){
+            return number;
+        } else {
+            throw new ValidateNumberException("number out of bounds");
+        }
+    }
+    public static float validateFloat(float number, float min, float limit) throws ValidateNumberException {
+        if (number >= min && number <= limit){
+            return number;
+        } else {
+            throw new ValidateNumberException("number out of bounds");
+        }
+    }
+
+
+}
