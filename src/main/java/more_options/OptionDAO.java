9e6d059c031bf9680f02496cb00e59b9c86531b1
gklogodazhki <gklogodazhki@gmail.com>
2017-10-14 20:44:26 +0300

    add OptionDAO class

---
+++ src/main/java/more_options/OptionDAO.java
@@ -0,0 +1,12 @@
+package more_options;
+
+import exceptions.ValidateStringException;
+import id_name_tables.IdNameDAO;
+
+public class OptionDAO extends IdNameDAO {
+
+    public OptionDAO() throws ValidateStringException {
+        super("more_options");
+    }
+
+}
