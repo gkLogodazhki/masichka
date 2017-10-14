95126f2e886bfa5c2d0caace5bb198bb048cad86
gklogodazhki <gklogodazhki@gmail.com>
2017-10-14 14:17:56 +0300

    add PlacesDAO class

---
+++ src/main/java/places/PlacesDAO.java
@@ -0,0 +1,92 @@
+package places;
+
+import database_access.DatabaseAccessManager;
+import exceptions.PlacesInfoException;
+
+import java.io.FileNotFoundException;
+import java.sql.*;
+
+public class PlacesDAO {
+
+    /*
+    Table: places
+        Columns:
+        id	int(11) AI PK
+        name	varchar(45)
+     */
+
+    private static final String INSERT_INTO_PLACES_INFO_SQL = "INSERT INTO places VALUES (null,?);";
+    private static final String SELECT_ID_FROM_PLACES_BY_NAME_SQL = "SELECT id FROM places WHERE name = ?;";
+    private static final String SELECT_NAME_FROM_PLACES_BY_ID_SQL = "SELECT name FROM places WHERE id = ?;";
+    private static final String SELECT_COUNT_FROM_PLACES_SQL = "SELECT count(id) counter FROM places;";
+    private static final String SELECT_NAMES_FROM_PLACES_SQL = "SELECT name FROM places";
+
+    public int insertIntoPlaces(PlacesPOJO places) throws FileNotFoundException, PlacesInfoException {
+        Connection connection = DatabaseAccessManager.getInstance().getConnection();
+
+        try {
+            PreparedStatement ps = connection.prepareStatement(INSERT_INTO_PLACES_INFO_SQL, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, places.getName());
+            ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+            rs.next();
+            return rs.getInt(1);
+
+        } catch (SQLException e) {
+            throw new PlacesInfoException("You can't insert into table places right now. Please try again later or connect with our supports");
+        }
+    }
+
+    public int getIdByName(PlacesPOJO places) throws FileNotFoundException, PlacesInfoException {
+        Connection connection = DatabaseAccessManager.getInstance().getConnection();
+
+        try {
+            PreparedStatement ps = connection.prepareStatement(SELECT_ID_FROM_PLACES_BY_NAME_SQL);
+            ps.setString(1, places.getName());
+            ResultSet rs = ps.executeQuery();
+            rs.next();
+            return rs.getInt(1);
+        } catch (SQLException e) {
+            throw new PlacesInfoException("You can't connect to table places right now. Please try again later or connect with our supports");
+        }
+    }
+
+    public String getNameById(PlacesPOJO places) throws FileNotFoundException, PlacesInfoException {
+        Connection connection = DatabaseAccessManager.getInstance().getConnection();
+
+        try {
+            PreparedStatement ps = connection.prepareStatement(SELECT_NAME_FROM_PLACES_BY_ID_SQL);
+            ps.setInt(1, places.getId());
+            ResultSet rs = ps.executeQuery();
+            rs.next();
+            return rs.getString("name");
+        } catch (SQLException e) {
+            throw new PlacesInfoException("You can't connect to table places right now. Please try again later or connect with our supports");
+        }
+    }
+
+    public ResultSet getNames() throws FileNotFoundException, PlacesInfoException {
+        Connection connection = DatabaseAccessManager.getInstance().getConnection();
+
+        try {
+            Statement statement = connection.createStatement();
+            return statement.executeQuery(SELECT_NAMES_FROM_PLACES_SQL);
+        } catch (SQLException e) {
+            throw new PlacesInfoException("You can't connect to table places right now. Please try again later or connect with our supports");
+        }
+    }
+
+    public int getCount() throws FileNotFoundException, PlacesInfoException {
+        Connection connection = DatabaseAccessManager.getInstance().getConnection();
+
+        try {
+            ResultSet rs = connection.createStatement().executeQuery(SELECT_COUNT_FROM_PLACES_SQL);
+            rs.next();
+            return rs.getInt("counter");
+        } catch (SQLException e) {
+            throw new PlacesInfoException("You can't connect to table places right now. Please try again later or connect with our supports");
+        }
+    }
+
+
+}
